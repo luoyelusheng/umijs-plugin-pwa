692d622750f696ce386be7007407bc18a4a53ff3
luoyelusheng <luoyelusheng@gmail.com>
2020-10-08 20:24:42 +0800

    fix: 修复dev环境生成问题

--- src/index.ts
+++ src/index.ts
@@ -31,95 +31,97 @@ export default function(api: IApi) {
     },
   });
 
-  // api.addEntryImports(async () => {
-  //   return [
-  //     {
-  //       source: `${__dirname}/registerServiceWorker.js`,
-  //       specifier: 'registerServiceWorker',
-  //     },
-  //   ];
-  // });
+  if (process.env.NODE_ENV === 'production') {
+    // api.addEntryImports(async () => {
+    //   return [
+    //     {
+    //       source: `${__dirname}/registerServiceWorker.js`,
+    //       specifier: 'registerServiceWorker',
+    //     },
+    //   ];
+    // });
 
-  api.addHTMLHeadScripts(async () => {
-    await cpy(`${__dirname}/registerServiceWorker.js`, `${absOutputPath}`);
-    await cpy(`${__dirname}/pwacompat.min.js`, `${absOutputPath}`);
-    return [
-      {
-        src: `./pwacompat.min.js`,
-        async: true,
-      },
-      {
-        src: `./registerServiceWorker.js`,
-      },
-    ];
-  });
+    api.addHTMLHeadScripts(async () => {
+      await cpy(`${__dirname}/registerServiceWorker.js`, `${absOutputPath}`);
+      await cpy(`${__dirname}/pwacompat.min.js`, `${absOutputPath}`);
+      return [
+        {
+          src: `./pwacompat.min.js`,
+          async: true,
+        },
+        {
+          src: `./registerServiceWorker.js`,
+        },
+      ];
+    });
 
-  api.onBuildComplete(async ({ err }) => {
-    if (!err) {
-      const src: string = options.src;
-      let manifestSrc: string = `${__dirname}/manifest.json`;
+    api.onBuildComplete(async ({ err }) => {
+      if (!err) {
+        const src: string = options.src;
+        let manifestSrc: string = `${__dirname}/manifest.json`;
 
-      if (src) {
-        manifestSrc = `${absSrcPath}/${src}`;
+        if (src) {
+          manifestSrc = `${absSrcPath}/${src}`;
 
-        const extension = src.substring(src.lastIndexOf('.') + 1);
+          const extension = src.substring(src.lastIndexOf('.') + 1);
 
-        assert(
-          extension === 'json',
-          `The manifest file must be a ${chalk.underline.cyan(
-            'json',
-          )} file，Your current file type is ${chalk.underline.cyan(
-            extension,
-          )}.`,
+          assert(
+            extension === 'json',
+            `The manifest file must be a ${chalk.underline.cyan(
+              'json',
+            )} file，Your current file type is ${chalk.underline.cyan(
+              extension,
+            )}.`,
+          );
+        }
+
+        const defaultManifest = await loadJsonFile(manifestSrc);
+
+        await cpy(manifestSrc, `${absOutputPath}`, {
+          rename: () => 'manifest.json',
+        });
+
+        const mergeManifest = Object.assign(
+          {},
+          defaultManifest,
+          options.manifest,
         );
+
+        await writeJsonFile(`${absOutputPath}/manifest.json`, mergeManifest);
       }
+    });
 
-      const defaultManifest = await loadJsonFile(manifestSrc);
+    api.addHTMLMetas(() => {
+      return [
+        {
+          name: 'apple-mobile-web-app-status-bar-style',
+          content: options.appStatusBar,
+        },
+      ];
+    });
 
-      await cpy(manifestSrc, `${absOutputPath}`, {
-        rename: () => 'manifest.json',
-      });
+    api.addHTMLLinks(() => {
+      let href: string = './manifest.json';
+      if (options.hash) {
+        href += '?v=' + v;
+      }
+      return [
+        {
+          rel: 'manifest',
+          type: 'json',
+          href,
+        },
+      ];
+    });
 
-      const mergeManifest = Object.assign(
-        {},
-        defaultManifest,
-        options.manifest,
-      );
-
-      await writeJsonFile(`${absOutputPath}/manifest.json`, mergeManifest);
-    }
-  });
-
-  api.addHTMLMetas(() => {
-    return [
-      {
-        name: 'apple-mobile-web-app-status-bar-style',
-        content: options.appStatusBar,
-      },
-    ];
-  });
-
-  api.addHTMLLinks(() => {
-    let href: string = './manifest.json';
-    if (options.hash) {
-      href += '?v=' + v;
-    }
-    return [
-      {
-        rel: 'manifest',
-        type: 'json',
-        href,
-      },
-    ];
-  });
-
-  api.chainWebpack(config => {
-    config.plugin('workbox').use(GenerateSW, [
-      {
-        swDest: 'sw.js',
-        importWorkboxFrom: 'local',
-      },
-    ]);
-    return config;
-  });
+    api.chainWebpack(config => {
+      config.plugin('workbox').use(GenerateSW, [
+        {
+          swDest: 'sw.js',
+          importWorkboxFrom: 'local',
+        },
+      ]);
+      return config;
+    });
+  }
 }
